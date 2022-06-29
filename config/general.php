b190b8b5255ce845b7bb4cd5454a1465b94c2191
Tim Kelty <tim@craftcms.com>
2022-06-28 21:38:53 -0400

    4 update

--- config/general.php
+++ config/general.php
@@ -1,37 +1,10 @@
 <?php
-/**
- * General Configuration
- *
- * All of your system's general configuration settings go in here. You can see a
- * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
- *
- * @see \craft\config\GeneralConfig
- */
-
 use craft\helpers\App;
 
-$isProd = App::env('CRAFT_ENVIRONMENT') === 'production';
-$isDev = !$isProd;
-
 return [
-    // Whether generated URLs should omit "index.php"
     'omitScriptNameInUrls' => true,
-
-    // The secure key Craft will use for hashing and encrypting data
-    'securityKey' => App::env('CRAFT_SECURITY_KEY'),
-
-    // Whether Dev Mode should be enabled (see https://craftcms.com/guides/what-dev-mode-does)
-    'devMode' => $isDev,
-
-    // Whether administrative changes should be allowed
-    'allowAdminChanges' => $isDev,
-
-    // Whether crawlers should be allowed to index pages and following links
-    'disallowRobots' => !$isProd,
-
-    // 'disabledPlugins' => '*',
-
     'aliases' => [
-        '@web' => App::env('DEFAULT_SITE_URL')
+        '@web' => App::env('WEB_URL'),
+        '@webroot' => App::env('WEBROOT_PATH'),
     ]
 ];
